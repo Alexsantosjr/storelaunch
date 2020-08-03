705e36aa82dd28ee00ebf0f933d74ff9abc90a79
Alexandre <alexsantsjrr@gmail.com>
2020-08-03 16:47:22 -0300

    Debug Aplication

--- public/script.js
+++ public/script.js
@@ -1,15 +1,15 @@
 const input = document.querySelector('input[name="price"]')
-input.addEventListener("keydown", function(e){
+input.addEventListener("keydown", function(e) {
     
 
-    setTimeout(function(){
+    setTimeout(function() {
         let { value } = e.target
 
-        value = value.replace(/\D/g, "")
+        value = value.replace(/\D/g,"")
 
-        value = new.Intl.NumberFormat('pt-BR', {
-            style: "currency",
-            currenry: 'BRL'
+        value = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
         }).format(value/100)
 
         e.target.value = value
